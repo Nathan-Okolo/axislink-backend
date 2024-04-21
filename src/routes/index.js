003f36ff470f9d08257fe89486f0a44cb665a540
Nathan-Okolo <okolonathanchinedu1@gmail.com>
2024-04-21 19:24:35 +0100

    new update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ import uploadRoute from "../uploads/uploadImg.js";
 import authRoute from "./authentication/authRoutes.js";
 import postRoute from "./postRoutes/postRoutes.js";
 import userRoute from "./userServices/userServicesRoutes.js";
+const upload = await uploadRoute(); // Call the async function to get the router
 
 export default (router) => {
   router.use("/auth", authRoute());
   router.use("/profile", userRoute());
   router.use("/post", postRoute());
-  router.use("/upload", uploadRoute());
+  router.use("/upload", upload);
 
   return router;
 };
